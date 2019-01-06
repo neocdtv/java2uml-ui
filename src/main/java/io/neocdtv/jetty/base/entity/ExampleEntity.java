48a7f1166ba87dd9b6b9e324a8318eb2b9bfe9e3
neocdtv <krzysztof.wolf.1980@gmail.com>
2019-01-06 15:55:41 +0100

    init

---
+++ src/main/java/io/neocdtv/jetty/base/entity/ExampleEntity.java
@@ -0,0 +1,19 @@
+package io.neocdtv.jetty.base.entity;
+
+/**
+ * ExampleEntity.
+ *
+ * @author xix
+ * @since 22.12.17
+ */
+public class ExampleEntity {
+  private String exampleField;
+
+  public String getExampleField() {
+    return exampleField;
+  }
+
+  public void setExampleField(String exampleField) {
+    this.exampleField = exampleField;
+  }
+}
